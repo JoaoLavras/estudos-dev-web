0cf2b067c8ea8ed4fcc8e98a63b64edc7536dc69
JoaoLavras <joaoantoniogoncalves14@gmail.com>
2025-12-30 22:20:04 -0300

    JS: Calculadora finalizada

--- 01-front-end-fundamentos/04-projetos/003-calculadora/script.js
+++ 01-front-end-fundamentos/04-projetos/003-calculadora/script.js
@@ -1,16 +1,97 @@
 const tela = document.querySelector("#tela");
 const teclas = document.querySelectorAll(".tecla");
 
-//teclas vira um array de tecla
+let num = 0;
+let ultimaOperacao = "";
 
 function modificarTela(tecla){
-    tela.textContent += tecla.textContent;
+    const valor = tecla.textContent;
+
+    if(valor=="C"){
+        limpaTelaC();
+        return;
+    }
+
+    if(["+","-","x",":","="].includes(valor)){
+        tratarOperacao(valor);
+        return;
+    }
+
+    formarNumero(valor);
+}
+
+function tratarOperacao(op){
+    if(tela.textContent === "") return;
+
+    if(op === "="){
+        garanteUltimaOperacao();
+        ultimaOperacao = "";
+        return;
+    }
+
+    if(num === 0 && ultimaOperacao === ""){
+        num = parseFloat(tela.textContent);
+    }
+    else{
+        garanteUltimaOperacao();
+    }
+
+    ultimaOperacao = mapOperacao(op);
+    limpaTela();
+}
+
+function mapOperacao(op){
+    if(op === "+") return "sum";
+    if(op === "-") return "sub";
+    if(op === "x") return "mtp";
+    if(op === ":") return "div";
+    return "";
 }
 
 function clicarTecla(tecla){
     tecla.addEventListener("click",() => modificarTela(tecla));
 }
 
-teclas.forEach(clicarTecla); //aqui ele passa o parametro automaticamente
+function formarNumero(valor){
+    tela.textContent += valor;
+}
 
-//falta concluir algumas coisas 
+function limpaTelaC(){
+    tela.textContent = "";
+    num = 0;
+    ultimaOperacao = "";
+}
+
+function limpaTela(){
+    tela.textContent = "";
+}
+
+function mostrarNumero(){
+    tela.textContent = num;
+}
+
+function garanteUltimaOperacao(){
+
+    const valorAtual = parseFloat(tela.textContent);
+
+    switch(ultimaOperacao){
+        case "sum":
+            num += valorAtual;
+            break;
+
+        case "sub":
+            num -= valorAtual;
+            break;
+        case "div":
+            num /= valorAtual;
+            break;
+        case "mtp":
+             num *= valorAtual;
+             break;
+    }
+
+    mostrarNumero();
+
+}
+
+teclas.forEach(clicarTecla);
